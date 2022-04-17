5da45fce64cc7d8c8abfb4d7bc73ac9bc5f6ebe7
Yassine Jarray <yassinejarray312@gmail.com>
2022-04-17 15:16:52 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from configurations import Configuration
 from configurations import values
 import dj_database_url
+import hashlib
 
 class Dev(Configuration):
   # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -186,3 +187,9 @@ class Prod(Dev):
         "level": "DEBUG",
     },
     }
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
