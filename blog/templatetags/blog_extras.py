db38885d6707c379b99998af04c9cdd027c65b50
Yassine Jarray <yassinejarray312@gmail.com>
2022-04-18 17:27:48 +0000

    Finish custom User model

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -55,6 +55,13 @@ def endrow():
 def row(extra_classes=""):
     return format_html('<div class="row {}">', extra_classes)
 
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}"', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html('</div>')
 @register.simple_tag(takes_context=True)
 def author_details_tag(context):
     request = context["request"]
